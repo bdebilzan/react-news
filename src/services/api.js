2d16f038eccf9eefa12c54db3735d35ba77d094d
bdebilzan <bdebilzan@mac.com>
2025-03-06 18:00:32 -0800

    fix 426 error

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,16 @@
 const NEWS_KEY = import.meta.env.VITE_NEWS_API_KEY;
 
-export const getNews = async (category) => {
+export const News = async (category) => {
   const url = `https://newsapi.org/v2/top-headlines?category=${encodeURIComponent(
     category
   )}&apiKey=${NEWS_KEY}`;
-  const response = await fetch(url);
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      Connection: "keep-alive",
+      "Upgrade-Insecure-Requests": "1",
+    },
+  });
   const data = await response.json();
   return data.articles;
 };
